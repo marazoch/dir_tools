130d5ce019dab23a708b3ba16193c9074f191622
Andrey Stupin <rathergood13@gmail.com>
2025-07-28 18:44:12 +0300

    add metavar to clear help

--- manager.py
+++ manager.py
@@ -15,14 +15,14 @@ def main():
         description='simple file manager',
         epilog='for more information visit https://github.com/marazoch/dir_tools'
     )
-    subparsers = parser.add_subparsers(dest='command', required=True)
+    subparsers = parser.add_subparsers(dest='command', required=True, metavar='command')
 
     p = subparsers.add_parser('copy', help='Copy file')
-    p.add_argument('-s', '--src', required=True, help='file source')
-    p.add_argument('-d', '--dst', required=True, help='copy destination')
+    p.add_argument('-s', '--src', required=True, metavar='', help='file source')
+    p.add_argument('-d', '--dst', required=True, metavar='', help='copy destination')
 
     p = subparsers.add_parser('delete', help='Delete file or dir')
-    p.add_argument('-s', '--src', required=True, help='file source')
+    p.add_argument('-s', '--src', required=True, metavar='', help='file source')
 
     p = subparsers.add_parser('count', help='Count files')
     p.add_argument('-p', '--path', required=True, metavar='', help='path to file')
